c36a22a3e51ad287649063307701eae5d32fe038
Jake Coombs <57802613+jakecoombs@users.noreply.github.com>
2022-02-09 13:31:17 +0000

    nextLaunchReminder

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,8 @@ const db = admin.firestore();
 
 const dbTokenCollection = db.doc(functions.config().f1bot.token_collection);
 
+const twitterId = "1489655840156045323";
+
 const TwitterApi = require("twitter-api-v2").default;
 const twitterClient = new TwitterApi({
   clientId: functions.config().f1bot.client_id,
@@ -20,6 +22,20 @@ function presentableDate(date) {
   const dateString = date.toDateString();
   const lastIndex = dateString.lastIndexOf(" ");
   return dateString.substring(0, lastIndex);
+}
+
+async function minutesTweetedAgo(client) {
+  const now = new Date();
+  const tweets = await client.v2.userTimeline(
+      twitterId, {
+        "tweet.fields": "created_at",
+        "exclude": ["replies", "retweets"],
+        "max_results": 5,
+      },
+  );
+  const latest = tweets.tweets[0];
+  const latestCreatedAt = new Date(latest.created_at);
+  return Math.round(Math.abs(now - latestCreatedAt) / 60000);
 }
 
 // Step 1
@@ -150,6 +166,86 @@ exports.dailyLaunchReminder = functions.https.onRequest(
           }
         }
       }
+
+      const {data} = await refreshedClient.v2.tweet(msg);
+      response.send(data);
+    },
+);
+
+exports.nextLaunchReminder = functions.https.onRequest(
+    async (request, response) => {
+      const {refreshToken} = (await dbTokenCollection.get()).data();
+
+      const {
+        client: refreshedClient,
+        accessToken,
+        refreshToken: newRefreshToken,
+      } = await twitterClient.refreshOAuth2Token(refreshToken);
+
+      await dbTokenCollection.set({accessToken, refreshToken: newRefreshToken});
+
+      let msg = "";
+      const today = new Date();
+      const tomorrow = new Date(today);
+      tomorrow.setDate(today.getDate() + 1);
+      tomorrow.setHours(0, 0, 0, 0);
+
+      // if before final livery reveal, announce livery reminder
+      const nextLiveryReveal = await db
+          .collection("liveries")
+          .orderBy("date")
+          .where("date", ">", today)
+          .where("date", "<", tomorrow)
+          .limit(1)
+          .get();
+
+      const tweetedAgo = await minutesTweetedAgo(refreshedClient);
+
+      if (nextLiveryReveal.empty) {
+        return response.send(200);
+      }
+
+      const doc = nextLiveryReveal.docs[0].data();
+
+      // Announce upcoming livery reveals
+      msg = "🚨 F1 Car Launch Reminder 🚨\n\n";
+
+      // Show the time of the launch
+      msg += `${doc.team} - ${doc
+          .date.toDate()
+          .toLocaleTimeString()} GMT\n`;
+
+      // How long left to go
+      const hours = Math.round(Math.abs(doc.date.toDate() - today) / 36e5);
+
+      // If too long away then do not tweet
+      if (hours > 3) {
+        return response.send(200);
+      }
+
+      // Build tweet
+      if (hours < 1) {
+        const mins = Math.round(Math.abs(doc.date.toDate() - today) / 60000);
+        if (mins > 36) {
+          return response.send(200);
+        } else if (mins > 10) {
+          msg += `${mins} Minutes To Go`;
+        } else {
+          msg += "Launch is happening now!";
+        }
+      } else if (tweetedAgo > 59) {
+        msg += `${hours} Hour(s) To Go`;
+      } else {
+        return response.send(200);
+      }
+
+      // Attach link if available
+      if (doc.link) {
+        msg += `\n\nLink - ${doc.link}`;
+      }
+
+      // @ Twitter account
+      msg += `\n\n@${doc.twitter_handle}\n#F1 #${doc.team.replace(/\s+/g, "")}`;
 
       const {data} = await refreshedClient.v2.tweet(msg);
       response.send(data);
